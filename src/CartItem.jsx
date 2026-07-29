5ac590f595987d91edd5912a3665fb2846603af0
Irfan <irfanfauzan88@yahoo.com>
2026-07-29 13:15:46 -0400

    Finished Task 5

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,6 +21,10 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+
 
 
   const handleIncrement = (item) => {
@@ -69,7 +73,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
